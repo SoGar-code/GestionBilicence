73c848641d8c8503f2ff581f6d7faa7b93d6008a
Trivy <olivier.gabriel.geom@gmail.com>
2017-07-25 08:08:57 +0200

    Now with a file selector!

--- GestionBilicence/src/gestionBilicence/csvImport/CsvImport.java
+++ GestionBilicence/src/gestionBilicence/csvImport/CsvImport.java
@@ -1,14 +1,34 @@
 package gestionBilicence.csvImport;
+
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 /**
  * 
  * @author Trivy
  * A class to import CSV files
  */
-public class CsvImport {
+public class CsvImport extends JDialog{
 	AbstractImportReader air = new SimpleImportReader();
 	
+	public CsvImport(){
+		super();
+	}
+
 	public void importCsv(){
-		air.importReader("E:\\Data\\bilicence-notes_hors_rattrapage.csv");
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				"*.csv", "*.*");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(this);
+		String path = "";
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			path = chooser.getSelectedFile().getAbsolutePath();
+			System.out.println("You chose to open this file: " +
+					path);
+		}
+		air.importReader(path);
 		System.out.println("CsvImport.importCsv completed!");
 	}
 }
